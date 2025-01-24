a86b5fbdde64c5d67959af459b56826a934b7de3
embunns <115136455+embunns@users.noreply.github.com>
2025-01-24 23:39:58 +0700

    Delete flutter_application_w5 directory

--- flutter_application_w5/lib/routes/routes.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import '../pages/register_page.dart';
-
-class AppRoutes {
-  static final routes = [
-    GetPage(name: '/register', page: () => RegisterView()),
-    // Tambahkan rute lainnya jika perlu
-  ];
-}
